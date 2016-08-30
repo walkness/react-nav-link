e533f5b513725c6f81ed3c56a4d1f9c7edb6e85b
Walker Angell <w@lker.co>
2016-08-30 12:28:13 -0400

    bootstrap4 compatibility

--- index.jsx
+++ index.jsx
@@ -1,31 +1,38 @@
 import React, { PropTypes } from 'react';
 import { routerShape } from 'react-router/lib/PropTypes';
 import { Link } from 'react-router';
+import classNames from 'classnames';
 
 
 const NavLink = (props, context) => {
   const { liClassName, callback, indexOnly, additionalRoutes, dropdown, ...opts } = props;
-  let liClassNameList = [];
-  if (liClassName)
-    liClassNameList.push(liClassName)
-  if (callback(context.router.isActive(props.to, indexOnly))) {
-    liClassNameList.push('active');
-  } else {
+
+  let active = callback(context.router.isActive(props.to, indexOnly));
+  if (!active) {
     for (const route of additionalRoutes) {
       if (context.router.isActive(route, indexOnly)) {
-        liClassNameList.push('active');
+        active = true;
         break;
       }
     }
   }
 
   return (
-    <li className={ liClassNameList.join(' ') }>
-      <Link {...opts}/>
+    <li
+      className={classNames(
+        'nav-item',
+        { [liClassName]: liClassName },
+        { active },
+      )}
+    >
+
+      <Link className='nav-link' {...opts} />
+
       { dropdown }
+
     </li>
-  )
-}
+  );
+};
 
 NavLink.propTypes = {
   liClassName: PropTypes.string,
@@ -33,16 +40,16 @@ NavLink.propTypes = {
   additionalRoutes: PropTypes.array,
   indexOnly: PropTypes.bool,
   callback: PropTypes.func,
-}
+};
 
 NavLink.defaultProps = {
   additionalRoutes: [],
   indexOnly: false,
   callback: (e) => e,
-}
+};
 
 NavLink.contextTypes = {
   router: routerShape,
-}
+};
 
 export default NavLink;
